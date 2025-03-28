adc613a1d025b03adaf6fc85d38ba2152ea0522b
Keni Royer Santiago <keni.santiago@iecm.mx>
2025-03-28 14:16:54 -0600

    Se integra la logica para el jugador PC

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,12 +10,14 @@ const typeCards = ['C', 'D', 'H', 'S'];
 const specials  = ['A', 'J', 'Q', 'K'];
 
 let pointsPlayer   = 0,
-    pointsComputer = 1;
+    pointsComputer = 0;
 
 //Events
-const btnPedir = document.querySelector('#btnPedir');
+const btnPedir   = document.querySelector('#btnPedir'),
+      btnDetener = document.querySelector('#btnDetener');
 
 const divPlayerDeck = document.querySelector('#player-deck');
+const divComputerDeck = document.querySelector('#pc-deck');
 const pointsHTML = document.querySelectorAll('small');
 
 const setDeck = () => {
@@ -27,7 +29,7 @@ const setDeck = () => {
     
     for(let typeCard of typeCards) {
         for(let special of specials) {
-            deck.push(typeCard+special);
+            deck.push(special+typeCard);
         }
     }
 
@@ -48,12 +50,40 @@ const getCard = () => {
     return card;
 }
 
-getCard()
-
 const cardScoring = (cardValue) => {
     const value = cardValue.substring(0, cardValue.length - 1);
     return (isNaN(value))  ? ((value === 'A') ? 11 : 10) : (value*1);  
 }
+
+//Turno de la computadora
+const  computerTurn = ( minPoints ) => {
+    do {
+            const requestCard = getCard();
+            pointsComputer = pointsComputer + cardScoring(requestCard);
+            pointsHTML[1].innerText = pointsComputer;
+
+            const imgCard = document.createElement('img');
+            imgCard.src = `assets/cards/${requestCard}.png`;
+            imgCard.classList.add('card-customize');
+            divComputerDeck.append(imgCard);
+            
+            if(minPoints > 21) {
+                break;
+            }
+
+    } while((pointsComputer < minPoints) && (minPoints <= 21))
+
+        setTimeout(()=>{
+            if(pointsComputer === minPoints) {
+                alert('Nadie Gana');
+            }else if (minPoints > 21) {
+                alert('Gana computadora')
+            }else if(pointsComputer >21){
+                alert('Jugador 1 gana');
+            }
+        },100)
+} 
+
 
 //Events
 btnPedir.addEventListener('click', () => {
@@ -67,10 +97,21 @@ btnPedir.addEventListener('click', () => {
     divPlayerDeck.append(imgCard);
 
     if(pointsPlayer > 21) {
-        alert('Has perdido')
         console.warn('Perdiste, rebasasete los 21 puntos');
-        btnPedir.ariaDisabled = true;
+        btnPedir.disabled   = true;
+        btnDetener.disabled = true;
+        computerTurn(pointsPlayer);
     } else if(pointsPlayer === 21) {
         alert('¡¡Has ganado!!')
+        btnPedir.disabled   = true;
+        btnDetener.disabled = true;
+        computerTurn(pointsPlayer);
     }
 });
+
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+
+    computerTurn(pointsPlayer);
+})
