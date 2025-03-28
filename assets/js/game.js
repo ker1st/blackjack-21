9d82c1f3bb6943891e0a03203a1b28528955f9de
Keni Royer Santiago <keni.santiago@iecm.mx>
2025-03-28 16:08:56 -0600

    se termina de desarrollar el juego 2

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,3 @@
-/**
- * 2C = Two of Clubs
- * 2D = Two of Diamonds
- * 2H = Two of Hearts
- * 2S = Two of Spades
- **/
-
 let deck        = [];
 const typeCards = ['C', 'D', 'H', 'S'];
 const specials  = ['A', 'J', 'Q', 'K'];
