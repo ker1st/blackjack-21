d490330d6a57544efdf7ba045a98060ee0a48cb7
Keni Royer Santiago <keni.santiago@iecm.mx>
2025-03-27 19:07:45 -0600

    Se agrega la logica de 21 al jugador

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,14 @@ let deck        = [];
 const typeCards = ['C', 'D', 'H', 'S'];
 const specials  = ['A', 'J', 'Q', 'K'];
 
+let pointsPlayer   = 0,
+    pointsComputer = 1;
+
+//Events
+const btnPedir = document.querySelector('#btnPedir');
+
+const divPlayerDeck = document.querySelector('#player-deck');
+const pointsHTML = document.querySelectorAll('small');
 
 const setDeck = () => {
     for(let i = 2; i <= 10; i++) {
@@ -43,12 +51,26 @@ const getCard = () => {
 getCard()
 
 const cardScoring = (cardValue) => {
-    console.log(cardValue);
     const value = cardValue.substring(0, cardValue.length - 1);
     return (isNaN(value))  ? ((value === 'A') ? 11 : 10) : (value*1);  
 }
 
+//Events
+btnPedir.addEventListener('click', () => {
+    const card = getCard();
+    pointsPlayer = pointsPlayer + cardScoring(card);
+    pointsHTML[0].innerText = pointsPlayer;
 
-const valor = cardScoring('JH');
+    const imgCard = document.createElement('img');
+    imgCard.src   = `assets/cards/${card}.png`;//arma la estructuras de las cartas
+    imgCard.classList.add('card-customize');
+    divPlayerDeck.append(imgCard);
 
-console.log(valor);
+    if(pointsPlayer > 21) {
+        alert('Has perdido')
+        console.warn('Perdiste, rebasasete los 21 puntos');
+        btnPedir.ariaDisabled = true;
+    } else if(pointsPlayer === 21) {
+        alert('¡¡Has ganado!!')
+    }
+});
